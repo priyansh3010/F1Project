93321fe053919e937434c96a86799fec5d5943b8
priyansh3010 <pri.obhan30@gmail.com>
2025-09-04 02:06:47 +0530

    Added more communicatio between React and Flask

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from names_and_ids import driver_ids, driver_names
+from names_and_ids import driver_ids, driver_names, track_ids, track_names
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for React
@@ -10,24 +10,34 @@ def data():
     return jsonify({"message": "Hi from Flask!"})
 
 @app.route("/api/driverIds")
-def get_driver_ids():    
+def get_driver_ids():
     return jsonify({"driverIds": driver_ids})
 
 @app.route("/api/driverNames")
 def get_driver_names():
-    temp = [1, 2, 3, 4, 7, 8, 9, 12, 13, 15, 17, 19, 20, 24, 25, 26, 27, 30, 31, 32]
+    drivers_ids_2025 = [1, 2, 3, 4, 7, 8, 9, 12, 13, 15, 17, 19, 20, 24, 25, 26, 27, 30, 31, 32]
     driver_names_2025 = {}
 
-    for id in temp:
+    for id in drivers_ids_2025:
         driver_names_2025[id] = driver_names[id]
 
     return jsonify({"driverNames": driver_names_2025})
+
+@app.route("/api/trackNames")
+def get_track_names():
+    track_ids_2025 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+    track_names_2025 = {}
+
+    for id in track_ids_2025:
+        track_names_2025[id] = track_names[id]
+
+    return jsonify({"trackNames" : track_names_2025})
 
 @app.route("/api/submitQualifying", methods=["POST"])
 def submitQualifying():
     data = request.get_json()
     print(data)
-    return(jsonify({"status": "Recieved"}))
+    return jsonify({"status": "Recieved"})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
--- frontend/src/DriverPosInputs.jsx
+++ frontend/src/DriverPosInputs.jsx
@@ -3,24 +3,59 @@ import { useEffect, useState } from 'react';
 function DriverPosInputs() {
 
     const [drivers, setDrivers] = useState({});
+    const [tracks, setTracks] = useState({});
     
         useEffect(() => {
             fetch("http://192.168.29.125:5000/api/driverNames")
             .then((response) => response.json())
             .then((data) => {
-                console.log(data.driverNames);
                 setDrivers(data.driverNames);
             })
             .catch((error) => console.error("Error fetching Driver IDs:", error));
+
+            fetch("http://192.168.29.125:5000/api/trackNames")
+            .then((response) => response.json())
+            .then((data) => {
+                setTracks(data.trackNames);
+            })
+            .catch((error) => console.error("Error fetching Track IDs:", error));
         }, []);
 
     const [driverQualifyingPos, setDriverQualifyingPos] = useState({});
+    const [gapsToPole, setGapsToPole] = useState({});
+    const [weather, setWeather] = useState({});
+    const [trackId, setTrackId] = useState();
 
     function handleDriverQualifyingPosChange(driverId, event) {
         setDriverQualifyingPos(prev => ({
             ...prev,
             [driverId] : event.target.value
         }));
+    }
+
+    function handleGapsToPole(driverId, event) {
+        setGapsToPole(prev => ({
+            ...prev,
+            [driverId] : event.target.value
+        }));
+    }
+
+    function handleGapToPole(driverId, event) {
+        setDriverQualifyingPos(prev => ({
+            ...prev,
+            [driverId] : event.target.value
+        }));
+    }
+
+    function handleWeatherChange(attribute, event) {
+        setWeather(prev => ({
+            ...prev,
+            [attribute] : event.target.value
+        }));
+    }
+
+    function handleTrackChange(id) {
+        setTrackId(t => t = id);
     }
 
     async function handleSubmit(event) {
@@ -32,7 +67,7 @@ function DriverPosInputs() {
                 headers : {
                     "Content-Type" : "application/json"
                 },
-                body : JSON.stringify(driverQualifyingPos)
+                body : JSON.stringify([driverQualifyingPos, gapsToPole, trackId, weather])
             });
 
             console.log(response);
@@ -40,7 +75,7 @@ function DriverPosInputs() {
             console.log(result);
         }
         catch (error) {
-            console.log("ERROR");
+            console.log(error);
         }
     }
 
@@ -50,31 +85,66 @@ function DriverPosInputs() {
             <h1>Driver Positions</h1>
         </div>
         <form onSubmit={handleSubmit}>
-        <div className="container">
-                {
-                    drivers ? Object.entries(drivers).map(([key, value]) => (
-                        <>
-                            <div className="card" key={key}>
-                                <p>{value}</p>
-                                <div className="input-container">
-                                    <p>Qualifying Position: </p>
-                                    <input type="number" onChange={e => handleDriverQualifyingPosChange(key, e)} value={driverQualifyingPos[key] || ""} />
+            <div className="container">
+                    {
+                        Object.entries(drivers).map(([key, value]) => (
+                            <>
+                                <div className="card" key={key}>
+                                    <p>{value}</p>
+                                    <div className="input-container">
+                                        <div>
+                                            <p>Qualifying Position: </p>
+                                            <input type="number" onChange={e => handleDriverQualifyingPosChange(key, e)} value={driverQualifyingPos[key] || ""} />
+                                        </div>
+                                        <div>
+                                            <p>Gap to Pole: </p>
+                                            <input type="number" onChange={e => handleGapsToPole(key, e)} value={gapsToPole[key] || ""} />
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </>
-                    )) : <>
-                            <div className="card">
-                                <p>Max Verstappen</p>
-                                <div className="input-container">
-                                    <p>Qualifying Position: </p>
-                                    <input type="number" name="" id="" />
-                                </div>
-                                <h1>Error!</h1>
-                            </div>
-                        </>
-                }
-        </div>
-        <button type="submit">Submit</button>
+                            </>
+                        ))
+                    }
+                    <div className="card">
+                        <p>Wind Speed</p>
+                        <div className="input-container">
+                            <input type="number" onChange={e => handleWeatherChange("wind-speed", e)} value={weather["wind-speed"] || ""} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <p>Apparent Temp</p>
+                        <div className="input-container">
+                            <input type="number" onChange={e => handleWeatherChange("apparent-temp", e)} value={weather["apparent-temp"] || ""} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <p>Relative Humidity</p>
+                        <div className="input-container">
+                            <input type="number" onChange={e => handleWeatherChange("relative-humidity", e)} value={weather["relative-humidity"] || ""} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <p>Rain Before</p>
+                        <div className="input-container">
+                            <input type="number" onChange={e => handleWeatherChange("rain-before", e)} value={weather["rain-before"] || ""} />
+                        </div>
+                    </div>
+                    <div className="card">
+                        <p>Rain During</p>
+                        <div className="input-container">
+                            <input type="number" onChange={e => handleWeatherChange("rain-during", e)} value={weather["rain-during"] || ""} />
+                        </div>
+                    </div>
+                    <select onChange={event => handleTrackChange(event.target.value)}>
+                        <option selected disabled>Select a track</option>
+                        {
+                            Object.entries(tracks).map(([key, value]) => (
+                                <option key={key} value={key}>{value}</option>
+                            ))
+                        }
+                    </select>
+            </div>
+            <button type="submit">Submit</button>
         </form>
     </>
   );
